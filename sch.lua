1f22d082a43a334a3141c940911055c281f5cf37
HCR-750F <54973190+sch-lda@users.noreply.github.com>
2023-06-30 12:52:33 +0800

    Update sch.lua

--- sch.lua
+++ sch.lua
@@ -1,21 +1,116 @@
+--------------------------------------------------------------------------------------- functions
+--[[
 function upgrade_vehicle(vehicle)
     for i = 0, 49 do
         local num = VEHICLE.GET_NUM_VEHICLE_MODS(vehicle, i)
         VEHICLE.SET_VEHICLE_MOD(vehicle, i, num - 1, true)
     end
 end
+]]
 
-function create_object(Hash, Pos)
-    STREAMING.REQUEST_MODEL(Hash)
-    while not STREAMING.HAS_MODEL_LOADED(Hash) do script.yield() end
-    local SpawnedVehicle = OBJECT.CREATE_OBJECT_NO_OFFSET(hash, Pos.x, Pos.y, Pos.z, true, false, false)
-    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(Hash)
+function create_object(hash, pos)
+    gui.show_message("Debughash", hash)
+   -- gui.show_message("DebugX", pos.x)
+    STREAMING.REQUEST_MODEL(hash)
+    while not STREAMING.HAS_MODEL_LOADED(hash) do script.yield() end
+    local obj = OBJECT.CREATE_OBJECT(hash, pos.x, pos.y, pos.z, true, false, false)
+    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(hash)
+    return obj
 end
+
+function request_model(hash)
+    local end_time = os.time() + 5
+    STREAMING.REQUEST_MODEL(hash)
+    while not STREAMING.HAS_MODEL_LOADED(hash) and end_time >= os.time() do
+        script.yield()
+    end
+    return STREAMING.HAS_MODEL_LOADED(hash)
+end
+
+function Create_Network_Ped(pedType, modelHash, x, y, z, heading)
+    request_model(modelHash)
+    local ped = PED.CREATE_PED(pedType, modelHash, x, y, z, heading, true, true)
+
+    ENTITY.SET_ENTITY_LOAD_COLLISION_FLAG(ped, true)
+    ENTITY.SET_ENTITY_AS_MISSION_ENTITY(ped, true, false)
+    ENTITY.SET_ENTITY_SHOULD_FREEZE_WAITING_ON_COLLISION(ped, true)
+
+    NETWORK.NETWORK_REGISTER_ENTITY_AS_NETWORKED(ped)
+    local net_id = NETWORK.PED_TO_NET(ped)
+    NETWORK.SET_NETWORK_ID_EXISTS_ON_ALL_MACHINES(net_id, true)
+    NETWORK.SET_NETWORK_ID_CAN_MIGRATE(net_id, true)
+    for _, pid in pairs(players.list()) do
+        NETWORK.SET_NETWORK_ID_ALWAYS_EXISTS_FOR_PLAYER(net_id, pid, true)
+    end
+
+    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(modelHash)
+    return ped
+end
+
+--------------------------------------------------------------------------------------- MPx
 
 local playerid = globals.get_int(1574918) --疑似与MPPLY_LAST_MP_CHAR相等
 
+local mpx ="MP0_"
+if playerid == 0 then 
+    mpx = "MP0_" 
+else 
+    mpx = "MP1_" 
+end
 
+--gui.show_message("Debugmpx", mpx.."H4_")
+---------------------------------------------------------------------------------------
+--[[
+gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("测试6", function()
 
+    local pos = ENTITY.GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(PLAYER.PLAYER_PED_ID(), 0.0, 0.52, 0.0)
+    gui.show_message("DebugX", pos.x)
+    create_object(200846641, pos)
+
+end)
+]]--
+--------------------------------------------------------------------------------------- Players 页面
+--------------------------------------------------------------------------------------- Players 页面
+gui.get_tab(""):add_separator()
+gui.get_tab(""):add_text("Lua选项") 
+
+gui.get_tab(""):add_button("栅栏笼子", function()
+    local objHash <const> = MISC.GET_HASH_KEY("prop_fnclink_03e")
+    STREAMING.REQUEST_MODEL(objHash)
+
+    local pos = ENTITY.GET_ENTITY_COORDS(PLAYER.GET_PLAYER_PED(network.get_selected_player()), false)
+
+    pos.z = pos.z - 1.0
+    local object = {}
+
+    object[1] = OBJECT.CREATE_OBJECT(objHash, pos.x - 1.5, pos.y + 1.5, pos.z,true, 1, 0)
+    object[2] = OBJECT.CREATE_OBJECT(objHash, pos.x - 1.5, pos.y - 1.5, pos.z,true, 1, 0)
+
+    object[3] = OBJECT.CREATE_OBJECT(objHash, pos.x + 1.5, pos.y + 1.5, pos.z,true, 1, 0)
+    local rot_3 = ENTITY.GET_ENTITY_ROTATION(object[3], 2)
+    rot_3.z = -90.0
+    ENTITY.SET_ENTITY_ROTATION(object[3], rot_3.x, rot_3.y, rot_3.z, 1, true)
+
+    object[4] = OBJECT.CREATE_OBJECT(objHash, pos.x - 1.5, pos.y + 1.5, pos.z,true, 1, 0)
+    local rot_4 = ENTITY.GET_ENTITY_ROTATION(object[4], 2)
+    rot_4.z = -90.0
+    ENTITY.SET_ENTITY_ROTATION(object[4], rot_4.x, rot_4.y, rot_4.z, 1, true)
+    ENTITY.IS_ENTITY_STATIC(object[1]) 
+    ENTITY.IS_ENTITY_STATIC(object[2])
+    ENTITY.IS_ENTITY_STATIC(object[3])
+    ENTITY.IS_ENTITY_STATIC(object[4])
+    ENTITY.SET_ENTITY_CAN_BE_DAMAGED(object[1], false) 
+    ENTITY.SET_ENTITY_CAN_BE_DAMAGED(object[2], false) 
+    ENTITY.SET_ENTITY_CAN_BE_DAMAGED(object[3], false) 
+    ENTITY.SET_ENTITY_CAN_BE_DAMAGED(object[4], false) 
+
+    for i = 1, 4 do ENTITY.FREEZE_ENTITY_POSITION(object[i], true) end
+    STREAMING.SET_MODEL_AS_NO_LONGER_NEEDED(objHash)
+
+end)
+
+--------------------------------------------------------------------------------------- Lua管理器页面
+--------------------------------------------------------------------------------------- Lua管理器页面
 
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_separator()
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_text("SCH LUA(测试)") 
@@ -29,43 +124,24 @@ end)
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_sameline()
 
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("一键配置前置(猎豹雕像)", function()
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_TARGET"), 5, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_BS_GEN"), 131071, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_BS_ENTR"), 63, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_APPROACH"), -1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_WEAPONS"), 1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_WEP_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_ARM_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_HEL_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_GOLD_C"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_GOLD_C_SCOPED"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_PAINT_SCOPED"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_PAINT"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_GOLD_V"), 585151, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_PAINT_V"), 438863, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4_PROGRESS"), 124271, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4_MISSIONS"), 65279, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_COKE_I_SCOPED"), 16777215, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_COKE_I"), 16777215, true)
-
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_TARGET"), 5, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_BS_GEN"), 131071, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_BS_ENTR"), 63, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_APPROACH"), -1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_WEAPONS"), 1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_WEP_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_ARM_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_HEL_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_GOLD_C"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_GOLD_C_SCOPED"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_PAINT_SCOPED"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_PAINT"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_GOLD_V"), 585151, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_PAINT_V"), 438863, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4_PROGRESS"), 124271, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4_MISSIONS"), 65279, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_COKE_I_SCOPED"), 16777215, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_COKE_I"), 16777215, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_TARGET"), 5, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_BS_GEN"), 131071, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_BS_ENTR"), 63, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_APPROACH"), -1, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_WEAPONS"), 1, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_WEP_DISRP"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_ARM_DISRP"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_HEL_DISRP"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_GOLD_C"), 255, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_GOLD_C_SCOPED"), 255, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_PAINT_SCOPED"), 127, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_PAINT"), 127, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_GOLD_V"), 585151, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_PAINT_V"), 438863, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4_PROGRESS"), 124271, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4_MISSIONS"), 65279, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_COKE_I_SCOPED"), 16777215, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_COKE_I"), 16777215, true)
 
     gui.show_message("写入完成", "远离计划面板并重新接近以刷新面板")
 end)
@@ -73,43 +149,24 @@ end)
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_sameline()
 
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("一键配置前置(粉钻)", function()
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_TARGET"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_BS_GEN"), 131071, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_BS_ENTR"), 63, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_APPROACH"), -1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_WEAPONS"), 1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_WEP_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_ARM_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4CNF_HEL_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_GOLD_C"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_GOLD_C_SCOPED"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_PAINT_SCOPED"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_PAINT"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_GOLD_V"), 585151, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_PAINT_V"), 438863, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4_PROGRESS"), 124271, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4_MISSIONS"), 65279, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_COKE_I_SCOPED"), 16777215, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_H4LOOT_COKE_I"), 16777215, true)
-
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_TARGET"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_BS_GEN"), 131071, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_BS_ENTR"), 63, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_APPROACH"), -1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_WEAPONS"), 1, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_WEP_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_ARM_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4CNF_HEL_DISRP"), 3, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_GOLD_C"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_GOLD_C_SCOPED"), 255, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_PAINT_SCOPED"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_PAINT"), 127, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_GOLD_V"), 585151, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_PAINT_V"), 438863, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4_PROGRESS"), 124271, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4_MISSIONS"), 65279, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_COKE_I_SCOPED"), 16777215, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_H4LOOT_COKE_I"), 16777215, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_TARGET"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_BS_GEN"), 131071, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_BS_ENTR"), 63, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_APPROACH"), -1, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_WEAPONS"), 1, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_WEP_DISRP"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_ARM_DISRP"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4CNF_HEL_DISRP"), 3, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_GOLD_C"), 255, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_GOLD_C_SCOPED"), 255, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_PAINT_SCOPED"), 127, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_PAINT"), 127, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_GOLD_V"), 585151, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_PAINT_V"), 438863, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4_PROGRESS"), 124271, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4_MISSIONS"), 65279, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_COKE_I_SCOPED"), 16777215, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."H4LOOT_COKE_I"), 16777215, true)
 
     gui.show_message("写入完成", "远离计划面板并重新接近以刷新面板")
 
@@ -134,7 +191,7 @@ gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("放烟花", function()
 
     script.sleep(1500)
 
-    local firework_box = OBJECT.CREATE_OBJECT(3176209716, pos.x,pos.y,pos.z, true, false, false)
+    local firework_box =     create_object(3176209716, pos)
     local firework_box_pos = ENTITY.GET_ENTITY_COORDS(firework_box)
 
     OBJECT.PLACE_OBJECT_ON_GROUND_PROPERLY(firework_box)
@@ -222,12 +279,9 @@ gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("夜总会保险箱30万循环10�
         gui.show_message("已执行次数", a2)
         globals.set_int(262145 + 24227,300000)
         globals.set_int(262145 + 24223,300000)
-        STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_CLUB_POPULARITY"), 10000, true)
-        STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_CLUB_PAY_TIME_LEFT"), -1, true)
-        STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_CLUB_POPULARITY"), 100000, true)
-        STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_CLUB_POPULARITY"), 10000, true)
-        STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_CLUB_PAY_TIME_LEFT"), -1, true)
-        STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_CLUB_POPULARITY"), 100000, true)
+        STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."CLUB_POPULARITY"), 10000, true)
+        STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."CLUB_PAY_TIME_LEFT"), -1, true)
+        STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."CLUB_POPULARITY"), 100000, true)
         gui.show_message("警告", "此方法仅用于偶尔小额恢复")
 
         script.sleep(10000)
@@ -254,8 +308,7 @@ gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("地堡满原材料", function()
 end)
 --[[  已被检测
 gui.get_tab("GUI_TAB_LUA_SCRIPTS"):add_button("移除赌场轮盘冷却", function()
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP0_LUCKY_WHEEL_NUM_SPIN"), 0, true)
-    STATS.STAT_SET_INT(MISC.GET_HASH_KEY("MP1_LUCKY_WHEEL_NUM_SPIN"), 0, true)
+    STATS.STAT_SET_INT(MISC.GET_HASH_KEY(mpx.."LUCKY_WHEEL_NUM_SPIN"), 0, true)
     globals.set_int(262145+27382,1) -- 9960150 
     globals.set_int(262145+27383,1) -- -312420223
 end)
